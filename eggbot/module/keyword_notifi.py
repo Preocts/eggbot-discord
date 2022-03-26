44f48947f85a39c2fe10e21b0d0d986df2eae105
Preocts <preocts@gmail.com>
2022-03-25 23:43:16 -0400

    Config work

--- eggbot/module/keyword_notifi.py
+++ eggbot/module/keyword_notifi.py
@@ -1,11 +1,58 @@
 """Alert, via DM, from bot when keyword is said in monitored channels."""
+import dataclasses
+import re
+from typing import Any
+
 from eggbot.model.chat_message import ChatMessage
+
+
+@dataclasses.dataclass(frozen=True)
+class KeywordNotifiConfig:
+    """Represents member configuration for Keyword Notifi module."""
+
+    member_id: str
+    pattern: re.Pattern[str]
+    enabled: bool
+    block_list: list[str]
 
 
 class KeywordNotifi:
     """Alert, via DM, from bot on keyword mention in chat message."""
 
+    config_section = "keyword_notifi"
+
+    def __init__(self) -> None:
+        self.config: dict[str, KeywordNotifiConfig] = {}
+
+    # Interface
     def process_message(self, message: ChatMessage) -> bool:
         """Process chat message."""
         # TODO: Build
         return False
+
+    # Interface
+    def load_config(self, config: dict[str, Any]) -> None:
+        """
+        Load config into class, removes existing loaded config values.
+
+        Args:
+            config: configuration mapping
+
+        Returns:
+            None
+        """
+        # Reset state config
+        self.config = {}
+
+        try:
+            members = config[self.config_section]
+        except KeyError as err:
+            raise KeyError(f"Config file missing expected key '{err}'") from err
+
+        for member in members:
+            self.config[member["member_id"]] = KeywordNotifiConfig(
+                member_id=member["member_id"],
+                pattern=re.compile(member["pattern"], re.I),
+                enabled=member["enabled"],
+                block_list=member["block_list"],
+            )
--- tests/module/keyword_notifi_test.py
+++ tests/module/keyword_notifi_test.py
@@ -1,14 +1,35 @@
+import random
+from typing import Any
 from typing import Generator
 
 import pytest
 
 from eggbot.model.chat_message import ChatMessage
 from eggbot.module.keyword_notifi import KeywordNotifi
+from eggbot.module.keyword_notifi import KeywordNotifiConfig
+
+random.seed()
+
+
+def mock_configs(size: int) -> dict[str, Any]:
+    """Mocks the return of a loaded config from the unwritten loader"""
+    config: dict[str, Any] = {"keyword_notifi": []}
+    for idx in range(size):
+        member = {
+            "member_id": f"123-{idx}",
+            "pattern": "{idx}",
+            "enabled": random.choice([True, False]),
+            "block_list": [],
+        }
+        config["keyword_notifi"].append(member)
+    return config
 
 
 @pytest.fixture
 def module() -> Generator[KeywordNotifi, None, None]:
-    yield KeywordNotifi()
+    module = KeywordNotifi()
+    module.load_config(mock_configs(10))
+    yield module
 
 
 @pytest.mark.parametrize(
@@ -24,3 +45,15 @@ def test_process_message(module: KeywordNotifi, message: str, expected: bool) ->
     result = module.process_message(chat)
 
     assert result is expected
+
+
+@pytest.mark.parametrize(
+    ("size"),
+    (1, 10, 100, 10_000),
+)
+def test_load_config(size: int, module: KeywordNotifi) -> None:
+    config = mock_configs(size)
+
+    module.load_config(config)
+
+    assert len(module.config) == size
