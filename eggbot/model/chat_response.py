5726a20bd65eda5ab863581ed1bacff5af6fb4cb
Preocts <preocts@gmail.com>
2022-03-27 13:44:05 -0400

    WIP - Chat Response model placeholder

---
+++ eggbot/model/chat_response.py
@@ -0,0 +1,7 @@
+"""Model used to pass response actions back from a chat module"""
+
+
+class ChatResponse:
+    """Model used to pass response actions back from a chat module"""
+
+    wip: bool = True
