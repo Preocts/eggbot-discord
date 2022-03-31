f3dd625069ccec4cc6dbf47df0ac5a0340ffc35d
Preocts <preocts@gmail.com>
2022-03-30 21:55:34 -0400

    CRUD methods for deferred task provider

--- eggbot/provider/deferred_task_db.py
+++ eggbot/provider/deferred_task_db.py
@@ -1,7 +1,18 @@
+from __future__ import annotations
+
+import datetime
+import json
+from typing import Any
+from uuid import uuid4
+
+from eggbot.model.deferred_task import DeferredTask
 from eggbot.provider.db_connector import DBConnection
 
 
 class DeferredTaskDB:
+
+    IntegrityError = DBConnection.IntegrityError
+
     def __init__(self, db_connection: DBConnection) -> None:
         """CRUD method layer for Deferred Task table"""
         self.dbconn = db_connection
@@ -11,8 +22,9 @@ class DeferredTaskDB:
     def _init_table(self) -> None:
         """Build table if needed"""
         sql = (
-            "CREATE TABLE IF NOT EXISTS deferred_task"
-            "(created_at TEXT, retry_at Text, event TEXT, attempts INT)"
+            "CREATE TABLE IF NOT EXISTS deferred_task (uid TEXT PRIMARY KEY, "
+            "created_at TEXT, retry_at TEXT, event_type TEXT, "
+            "event TEXT, attempts INT)"
         )
         cursor = self.dbconn.cursor()
         try:
@@ -20,3 +32,91 @@ class DeferredTaskDB:
             self.dbconn.commit()
         finally:
             cursor.close()
+
+    def row_count(self) -> int:
+        """Return total rows in deferred task table"""
+        sql = "SELECT COUNT(uid) FROM deferred_task"
+        cursor = self.dbconn.cursor()
+        try:
+            cursor.execute(sql)
+            return cursor.fetchone()[0]
+        finally:
+            cursor.close()
+
+    def save(self, event: dict[str, Any], retry_after: int = 0) -> None:
+        """
+        Save deferred task to database.
+
+        Args:
+            event: Deliverable event payload to event handlers
+            retry_after: Number of seconds to wait, from save, before retry is attempted
+
+            NOTE: retry_after only affects the first retry attempt
+            NOTE: `uid` within the event is used as a unique key. If missing
+                    a uuid is generated.
+
+        Returns:
+            None
+
+        Raises:
+            KeyError: Missing `event_type` key in event
+            DeferredTask.IntegrityError: Non-unique `uid` provided in event
+        """
+        now = datetime.datetime.utcnow()
+        after = now + datetime.timedelta(seconds=retry_after)
+        uid = event.get("uid") or str(uuid4())
+        event_type = event["event_type"]
+        sql = (
+            "INSERT INTO deferred_task (uid, created_at, retry_at, event_type, event, "
+            "attempts) VALUES (?, ?, ?, ?, ?, ?)"
+        )
+
+        cursor = self.dbconn.cursor()
+        try:
+            cursor.execute(sql, (uid, now, after, event_type, json.dumps(event), 0))
+            self.dbconn.commit()
+        finally:
+            cursor.close()
+
+    def get(self, event_type: str | None = None) -> list[DeferredTask]:
+        """
+        Return deferred tasks from database
+
+        Args:
+            event_type: Optional filter by event type otherwise return all
+        """
+        if event_type:
+            sql = "SELECT * FROM deferred_task WHERE event_type=?"
+            values = [event_type]
+        else:
+            sql = "SELECT * FROM deferred_task"
+            values = []
+
+        cursor = self.dbconn.cursor()
+        try:
+            cursor.execute(sql, values)
+            return self._to_model(cursor.fetchall())
+        finally:
+            cursor.close()
+
+    def delete(self, uid: str) -> None:
+        """
+        Delete row from deferred_task table by uid
+
+        Args:
+            uid: UID of row to delete
+
+        Returns:
+            None
+        """
+        sql = "DELETE FROM deferred_task WHERE uid=?"
+        cursor = self.dbconn.cursor()
+        try:
+            cursor.execute(sql, (uid,))
+            self.dbconn.commit()
+        finally:
+            cursor.close()
+
+    def _to_model(self, rows: list[list[Any]]) -> list[DeferredTask]:
+        """Convert rows into DeferredTask model."""
+        return [DeferredTask(*row) for row in rows]
--- tests/provider/deferred_task_db_test.py
+++ tests/provider/deferred_task_db_test.py
@@ -7,7 +7,7 @@ from eggbot.provider.db_connector import DBConnection
 from eggbot.provider.deferred_task_db import DeferredTaskDB
 
 DB_FILE = ":memory:"
-EXPECTED_COLUMNS = ["created_at", "retry_at", "event", "attempts"]
+EXPECTED_COLUMNS = ["uid", "created_at", "retry_at", "event_type", "event", "attempts"]
 TABLE_NAME = "deferred_task"
 
 
@@ -25,6 +25,12 @@ def test_init(provider: DeferredTaskDB) -> None:
     assert isinstance(provider.dbconn, DBConnection)
 
 
+def test_row_count(provider: DeferredTaskDB) -> None:
+    result = provider.row_count()
+
+    assert result == 0
+
+
 def test_table_create(provider: DeferredTaskDB) -> None:
     cursor = provider.dbconn.cursor()
     cursor.execute(f"SELECT * FROM {TABLE_NAME}")
@@ -33,3 +39,60 @@ def test_table_create(provider: DeferredTaskDB) -> None:
     result = set(col_names) - set(EXPECTED_COLUMNS)
 
     assert len(result) == 0
+
+
+@pytest.mark.parametrize(("row_count"), (1, 10, 100, 10_000))
+def test_save(provider: DeferredTaskDB, row_count: int) -> None:
+    task = {"event_type": "remind", "message": "get eggs"}
+
+    for _ in range(row_count):
+        provider.save(task, 100)
+
+    assert provider.row_count() == row_count
+
+
+def test_save_with_uid_error(provider: DeferredTaskDB) -> None:
+    task = {"event_type": "remind", "message": "get eggs", "uid": "0"}
+    provider.save(task)
+
+    with pytest.raises(provider.IntegrityError):
+        provider.save(task)
+
+
+def test_get_all(provider: DeferredTaskDB) -> None:
+    total = 10
+    task = {"event_type": "remind", "message": "get eggs"}
+    for _ in range(total):
+        provider.save(task)
+
+    results = provider.get()
+
+    assert len(results) == total
+
+
+def test_get_by_event_type(provider: DeferredTaskDB) -> None:
+    total = 10
+    task01 = {"event_type": "remind", "message": "get eggs"}
+    task02 = {"event_type": "announce", "message": "get eggs"}
+    for _ in range(total):
+        provider.save(task01)
+        provider.save(task02)
+
+    result01 = provider.get("remind")
+    result02 = provider.get("announce")
+
+    assert len(result01) == total
+    assert len(result02) == total
+
+
+def test_delete(provider: DeferredTaskDB) -> None:
+    uid = "test"
+    task = {"event_type": "remind", "message": "get eggs", "uid": uid}
+    provider.save(task)
+    prevalidate = provider.row_count()
+
+    provider.delete(uid)
+    validate = provider.row_count()
+
+    assert prevalidate == 1
+    assert validate == 0
